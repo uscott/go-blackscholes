cc86f0d40281ad6af57a7d0494e3bf08ddb693a0
US <uriel.scott@protonmail.com>
2021-02-02 05:26:04 -0600

    Create sim.go to price via simulation

---
+++ sim.go
@@ -0,0 +1,39 @@
+package blackscholes
+
+import (
+	"math/rand"
+	"sync"
+	"time"
+
+	"github.com/maurodelazeri/gaussian-distribution"
+)
+
+func BSPriceSim(v, t, x, k, r, q float64, o OptionType, n uint) float64 {
+
+	if !ValidOptionType(o) || n == 0 {
+		return nan()
+	}
+
+	rand.Seed(time.Now().UnixNano())
+
+	mu, wg := new(sync.Mutex), new(sync.WaitGroup)
+	stdg, sum, x0 := gaussian.NewGaussian(0, 1), 0.0, exp(-q*t)*x
+	m, s := x0*exp((r-0.5*v*v)*t), v*sqrt(t)
+
+	wg.Add(int(n))
+	for i := 0; i < int(n); i++ {
+		go func(i int) {
+			mu.Lock()
+			e := exp(s * stdg.Ppf(float64(i)-0.5+rand.Float64()))
+			x = m * e
+			sum += Intrinsic(0, x, k, 0, 0, o)
+			x = m / e
+			sum += Intrinsic(0, x, k, 0, 0, o)
+			mu.Unlock()
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+
+	return sum / float64(2*n+2)
+}
