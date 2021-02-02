3fa96596316085ed64e71ca97675f4e59ae6df07
US <uriel.scott@protonmail.com>
2021-02-02 11:42:06 -0600

    Use a more or less stratified simulation

--- sim.go
+++ sim.go
@@ -18,11 +18,12 @@ func BSPriceSim(v, t, x, k, r, q float64, o OptionType, n uint) float64 {
 	sum, x0 := 0.0, exp(-q*t)*x
 	m, s := x0*exp((r-0.5*v*v)*t), v*sqrt(t)
 
-	wg.Add(int(n))
-	for i := 0; i < int(n); i++ {
+	wg.Add(int(n - 1))
+	for i := 1; i < int(n); i++ {
 		go func(i int) {
 			mu.Lock()
-			e := exp(s * rand.NormFloat64())
+			u := (float64(i) - 0.5 + rand.Float64()) / float64(n)
+			e := exp(s * NormCDFInverse(u))
 			x = m * e
 			sum += Intrinsic(0, x, k, 0, 0, o)
 			x = m / e
@@ -32,6 +33,13 @@ func BSPriceSim(v, t, x, k, r, q float64, o OptionType, n uint) float64 {
 		}(i)
 	}
 	wg.Wait()
+
+	u := 0.5 * rand.Float64() / float64(n)
+	e := exp(s * NormCDFInverse(u))
+	x = m * e
+	sum += Intrinsic(0, x, k, 0, 0, o)
+	x = m / e
+	sum += Intrinsic(0, x, k, 0, 0, o)
 
 	return exp(-r*t) * sum / float64(2*n)
 }
