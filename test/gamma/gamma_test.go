34f34f088de49f328ccdbe0b4ad79e295acfa0b4
US <uriel.scott@protonmail.com>
2021-02-02 15:09:43 -0600

    Create gamma test

---
+++ test/gamma/gamma_test.go
@@ -0,0 +1,63 @@
+package gammatest
+
+import (
+	"math"
+	"testing"
+
+	bs "github.com/uscott/go-blackscholes"
+)
+
+func Test_Gamma(t *testing.T) {
+
+	const N = 15
+	eps := 10.0
+	var v, tau, x, k, r, q float64 = 0.5, 1, 80, 120, 0.1, 0.5
+	o := bs.Put
+
+	gamma := bs.BSGamma(v, tau, x, k, r, q, o)
+
+	t.Logf("\n$ Gamma = %6.2f\n", x*gamma)
+
+	for i := 0; i < N; i++ {
+
+		gammanum := bs.BSGammaNum(v, tau, x, k, r, q, o, eps)
+
+		if math.IsNaN(gammanum) {
+			t.Fatal("NaN")
+		}
+
+		err := x * (gammanum - gamma)
+
+		t.Logf(
+			"Epsilon = %10.4g, $ GammaNum = %6.2f, Error = %8.4f",
+			eps, x*gammanum, err,
+		)
+
+		eps /= 2
+	}
+
+	x, k = .1, .09
+
+	gamma = bs.BSGamma(v, tau, x, k, r, q, o)
+	eps = 10
+
+	t.Logf("\n$ Gamma = %6.2f\n", x*gamma)
+
+	for i := 0; i < N; i++ {
+
+		gammanum := bs.BSGammaNum(v, tau, x, k, r, q, o, eps)
+
+		if math.IsNaN(gammanum) {
+			t.Fatal("NaN")
+		}
+
+		err := x * (gammanum - gamma)
+
+		t.Logf(
+			"Epsilon = %10.4g, GammaNum = %6.2f, Error = %8.4f",
+			eps, x*gammanum, err,
+		)
+
+		eps /= 2
+	}
+}
