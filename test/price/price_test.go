33c12e947188ce90607817067762a12e22be51ff
US <uriel.scott@protonmail.com>
2021-02-02 11:55:28 -0600

    Don't use slice for errors

--- test/price/price_test.go
+++ test/price/price_test.go
@@ -1,6 +1,7 @@
 package pricetest
 
 import (
+	"math"
 	"testing"
 
 	"github.com/uscott/go-blackscholes"
@@ -11,8 +12,7 @@ func Test_Price(t *testing.T) {
 	v, tt, x, k, r, q := 0.5, 1.0/12, 100.0, 110.0, 0.01, 0.005
 	o := blackscholes.Call
 
-	N := 22
-	errors := make([]float64, N)
+	N := 20
 	price := blackscholes.BSPrice(v, tt, x, k, r, q, o)
 
 	t.Logf("Price = %.2f\n", price)
@@ -29,13 +29,13 @@ func Test_Price(t *testing.T) {
 		simprice := blackscholes.BSPriceSim(v, tt, x, k, r, q, o, n)
 		w, wprev := float64(n)/float64(n+nprev), float64(nprev)/float64(n+nprev)
 		simprice = w*simprice + wprev*simprev
-		errors[i-1] = simprice/price - 1
+		err := simprice/price - 1
 		nprev += n
 		simprev = simprice
 
 		t.Logf(
-			"Number of randoms = %10d,\tsim price = %6.2f,\terror = %8.3f %%\n",
-			nprev, simprice, 100*errors[i-1])
+			"Number of randoms = %8d,\tsim price = %6.2f,\t|error| = %8.3f %%\n",
+			nprev, simprice, 100*math.Abs(err))
 	}
 
 }
