5140d7ac2c21c203982dd8e13ca1c7b89427d531
US <uriel.scott@protonmail.com>
2021-02-02 08:37:13 -0600

    Create num.go and write difference approximation functions

---
+++ num.go
@@ -0,0 +1,80 @@
+package blackscholes
+
+func BSDeltaNum(v, t, x, k, r, q float64, o OptionType, eps float64) float64 {
+
+	if CheckPriceParams(t, x, k, o) != nil {
+		return nan()
+	}
+
+	e := abs(eps)
+	pu := BSPriceNoErrorCheck(v, t, x+e, k, r, q, o)
+
+	if x < e {
+		pd := ZeroUnderlyingBSPrice(t, k, r, o)
+		pm := BSPriceNoErrorCheck(v, t, x, k, r, q, o)
+		wd, wu := e/(e+x), x/(e+x)
+		return wu*(pu-pm)/(e+x) + wd*(pm-pd)/(e+x)
+	}
+
+	pd := BSPriceNoErrorCheck(v, t, x-e, k, r, q, o)
+
+	return (pu - pd) / 2 / e
+}
+
+func BSGammaNum(v, t, x, k, r, q float64, o OptionType, eps float64) float64 {
+
+	if CheckPriceParams(t, x, k, o) != nil {
+		return nan()
+	}
+
+	e := abs(eps)
+
+	if x < e {
+		puu := BSPriceNoErrorCheck(v, t, x+e, k, r, q, o)
+		pu := BSPriceNoErrorCheck(v, t, 2*x, k, r, q, o)
+		pm := BSPriceNoErrorCheck(v, t, x, k, r, q, o)
+		pd := ZeroUnderlyingBSPrice(t, k, r, o)
+		d := (x + e) * (x + e)
+		cuu, cu, cm, cd := 2*x/e/d, (e-x)/x/d, -2/e/x, 1/x/(e+x)
+		return cuu*puu + cu*pu + cm*pm + cd*pd
+	}
+	pu := BSPriceNoErrorCheck(v, t, x+e, k, r, q, o)
+	pd := BSPriceNoErrorCheck(v, t, x-e, k, r, q, o)
+	pm := BSPriceNoErrorCheck(v, t, x, k, r, q, o)
+
+	return (pu - 2*pm + pd) / (e * e)
+}
+
+func BSVegaNum(v, t, x, k, r, q float64, o OptionType, eps float64) float64 {
+
+	if CheckPriceParams(t, x, k, o) != nil {
+		return nan()
+	}
+
+	pu := BSPriceNoErrorCheck(v+eps, t, x, k, r, q, o)
+	pd := BSPriceNoErrorCheck(v-eps, t, x, k, r, q, o)
+
+	return (pu - pd) / 2 / eps
+}
+
+func BSThetaNum(v, t, x, k, r, q float64, o OptionType, eps float64) float64 {
+
+	if CheckPriceParams(t, x, k, o) != nil {
+		return nan()
+	}
+
+	e := abs(eps)
+
+	if t < e {
+		pu := Intrinsic(0, x, k, 0, 0, o)
+		pd := BSPriceNoErrorCheck(v, t+e, x, k, r, q, o)
+		pm := BSPriceNoErrorCheck(v, t, x, k, r, q, o)
+		wu, wd := e/(e+t), t/(e+t)
+		return wu*(pu-pm)/(e+t) + wd*(pm-pd)/(e+t)
+	}
+
+	pu := BSPriceNoErrorCheck(v, t-e, x, k, r, q, o)
+	pd := BSPriceNoErrorCheck(v, t+e, x, k, r, q, o)
+
+	return (pu - pd) / 2 / e
+}
