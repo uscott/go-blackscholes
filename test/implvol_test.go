0d7833df1f69f3606c76a66d3517de659c239796
Uriel Scott <uriel.scott@protonmail.com>
2024-03-18 00:25:57 -0700

    Add more implied vol tests

--- test/implvol_test.go
+++ test/implvol_test.go
@@ -11,13 +11,23 @@ import (
 func TestImpliedVol(t *testing.T) {
 
 	assert := assert.New(t)
+	tolerance := defaultTolerance
 
 	vol, err := blackscholes.ImpliedVol(0, 0, 0, 0, 0, 0, blackscholes.OptionType(' '))
 	assert.Error(err)
 	assert.True(math.IsNaN(vol))
 
 	vol, err = blackscholes.ImpliedVol(0, 0, 0, 0, 0, 0, blackscholes.Straddle)
-	tolerance := 1e-4
 	assert.NoError(err)
 	assert.InDelta(0, vol, tolerance)
+
+	vol, timeToExpiry, spot, strike, interestRate, dividendYield, _ := getTestParams()
+
+	for _, optionType := range []blackscholes.OptionType{blackscholes.Call, blackscholes.Put, blackscholes.Straddle} {
+		premium, err := blackscholes.Price(vol, timeToExpiry, spot, strike, interestRate, dividendYield, optionType)
+		assert.NoError(err)
+		impliedVol, err := blackscholes.ImpliedVol(premium, timeToExpiry, spot, strike, interestRate, dividendYield, optionType)
+		assert.NoError(err)
+		assert.InDelta(vol, impliedVol, tolerance)
+	}
 }
